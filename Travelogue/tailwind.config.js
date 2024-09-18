50db79042ab07b6e67089d61e8ac80659ad30471
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2024-09-18 16:33:30 +0700

    Create project

---
+++ Travelogue/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}","./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
