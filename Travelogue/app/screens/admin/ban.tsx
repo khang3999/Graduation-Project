4942132b7e59338c60c1c695f12f582cb76218a7
charcasbeos <tranthianhthu0711@gmail.com>
2024-10-03 17:01:55 +0700

    Base layout for admin: drawer, tabs of account, report, information locations

---
+++ Travelogue/app/screens/admin/ban.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Ban = () => {
+  return (
+    <View>
+      <Text>Ban</Text>
+    </View>
+  )
+}
+
+export default Ban
