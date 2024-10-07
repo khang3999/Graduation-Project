be0d3bb8f9c96c928e36d4ef8ed59a881228d1b8
DO NGOC HIEU <122579807+DoNgocHieu@users.noreply.github.com>
2024-10-08 02:00:25 +0700

    connect success firebase DB

---
+++ Travelogue/firebase/firebaseConfig.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from '@firebase/app';
+import { getAuth } from '@firebase/auth';
+import { getDatabase } from '@firebase/database'; 
+import { ref, set } from "@firebase/database";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXg7q7WhZy38dAadC-f6zEXtq-8FKV58A",
+  authDomain: "travelogue-abb82.firebaseapp.com",
+  databaseURL: "https://travelogue-abb82-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "travelogue-abb82",
+  storageBucket: "travelogue-abb82.appspot.com",
+  messagingSenderId: "722317549136",
+  appId: "1:722317549136:web:d428aea2ab08fc7425bb55",
+  measurementId: "G-H5NCLHNC83"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app); 
+
+export { auth, database, ref, set };
