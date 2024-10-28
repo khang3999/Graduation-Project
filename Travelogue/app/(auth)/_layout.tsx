030a92983570ca49943f7d89b9a7f4ce70922305
DO NGOC HIEU <122579807+DoNgocHieu@users.noreply.github.com>
2024-10-28 22:50:27 +0700

    fix token id user

--- Travelogue/app/(auth)/_layout.tsx
+++ Travelogue/app/(auth)/_layout.tsx
@@ -2,35 +2,29 @@ import RouterAuth from "@/app/(auth)/RouterAuth";
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { router } from "expo-router";
-import { auth } from "@/firebase/firebaseConfig";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Layout = () => {
   const [isAuth, setIsAuth] = useState(false);
-  
+
   useEffect(() => {
-    const checkAuth = async () => {
-      const user = auth.currentUser; 
-      console.log(user);
-      if (user) {
-        setIsAuth(true);
-      } else {
-        setIsAuth(false);
-      }
-      console.log(user ? "User is authenticated" : "User is not authenticated"); 
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem("userToken");  
+      setIsAuth(!!token);  
     };
-    checkAuth(); 
-  }, []); 
+
+    checkToken(); 
+  }, []);
 
   useEffect(() => {
     if (isAuth) {
-      router.push("/(tabs)/");
+      router.push("/(tabs)/");  
     }
   }, [isAuth]);
 
   return (
     <>
-      {!isAuth ? <RouterAuth /> : null}
+      {!isAuth ? <RouterAuth /> : null}  
     </>
   );
 };
