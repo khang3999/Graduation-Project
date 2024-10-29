aa235bcb6abc30c69a11ee724bb405d6943a0a71
PhucTran <tranhieuphuc12@gmail.com>
2024-10-29 22:39:59 +0700

    [Fix] add missing file

---
+++ Travelogue/app/(tabs)/(profiles)/searchResult.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useAccount } from '@/contexts/AccountProvider'
+import AvatarProfile from '@/components/profile/AvatarProfile';
+import { Gallery } from 'iconsax-react-native';
+import GalleryTabView from '@/components/profile/GalleryTabView';
+export default function searchResult() {
+    const {searchedAccountData} = useAccount();
+    
+  return (
+    <>
+        <AvatarProfile userData={searchedAccountData}/>
+        <GalleryTabView userId={searchedAccountData.id} isSearched={true}/>
+    </>
+  )
+}
